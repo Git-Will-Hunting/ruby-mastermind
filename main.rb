2e192cf0619087d6f6bf3c83e7694146bf7c0b59
git-will-hunting <58862067+Git-Will-Hunting@users.noreply.github.com>
2023-04-30 03:29:56 -0400

    Guesses are recorded working on feedback

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 # mastermind game
 # Author: git-will-hunting
 
@@ -11,21 +13,32 @@
 # The feedback is the number of correct colors and positions
 # The player wins if they guess the code in 12 turns
 
-# nouns: game, board, player, peg, turn_count, guess, code, feedback
-# verbs: get_guess, set_feedback, win, lose, play, start, end
+# nouns: Game, Board, Player, Peg, turn_count, guess, code, feedback, Key_peg
+# verbs: player_guess, code_response, win, lose, play, start, end, generate_code,
+# validate_guess, display, retry, quit, restart, help
 # Game class, handles the game loop and input
 class Game
-  def __init__
+  def initialize
     @turn_count = 0
-    @board = Board.new
-    @player = Player.new('Player 1')
-    @code = Mastermind.new
+    @mastermind = Mastermind.new
+    @board = Board.new(self, @mastermind)
+    @player = Player.new('Player 1', self, @board, @mastermind)
+    @current_player = @player
+    system 'clear' # clear the terminal
+    puts 'Welcome to Mastermind!'
+    @mastermind.generate_code
   end
 
+  attr_accessor :turn_count
+
+  # the game loop
   def play
-    while turn_count < 12
+    while @turn_count < 12
+      # display the board
+      @board.display
       # get the guess from the player
-      player.player_guess
+      @current_player.player_input
+      binding.pry
     end
     # check the guess against the code
     # set the feedback
@@ -33,45 +46,145 @@ class Game
     # if they have, end the game
     # if they haven't, increment the turn count
   end
+
+  # quit method to exit the game
+  def quit
+    puts 'Thanks for playing! Press enter to exit'
+    gets
+    exit
+  end
+
+  # help method - explains the rules of the game and how to play
+  def help
+    help_message = <<-HELP
+    Mastermind is a code breaking game. The code is a sequence of 4 colours
+    The colours are red, green, blue, yellow, orange, purple
+    The code can have duplicates
+    The code is randomly generated
+    The player has 12 turns to guess the code
+    After each guess, the player is given feedback
+    The feedback is the number of correct colors and positions
+    The player wins if they guess the code in 12 turns
+    The player can enter 'quit' or 'q' to quit the game.
+    The player can enter 'restart' or 'r' to restart the game.
+    The player can enter 'help' or 'h' to display this help menu again.
+    Please enter 4 colour names separated by spaces to make a guess.
+    Press 'Enter' to continue
+    HELP
+    puts help_message
+    gets
+    @board.display
+  end
+
+  def restart
+    # restart the game
+    game = Game.new
+    game.play
+  end
 end
 
 # Board class
 # creates the board as a series of rows of 4 columns
 class Board
   # should contain the board
-  def __init__
-    @board = array.new(12, array.new(4))
+  def initialize(game, mastermind)
+    @board = Array.new(12, Array.new(4))
+    @feedback = Array.new(12, Array.new(4))
+    @mastermind = mastermind
+    @game = game
+  end
+
+  # allow the board to be read
+  def display
+    # display the board with feedback beside it
+    # should be a series of rows of 8 columns
+    # should be displayed in the terminal
+    # should be updated after each turn
+    system 'clear' # clear the terminal
+    @board.each_with_index do |row, index|
+      puts "| #{row[0]} | #{row[1]} | #{row[2]} | #{row[3]} |-| #{@feedback[index]}|"
+    end
+  end
+
+  # receive guess method
+  def receive_guess(guess)
+    @board[@game.turn_count] = guess
+    receive_feedback(@mastermind.feedback)
+  end
+
+  # receive feedback method
+  def receive_feedback(feedback)
+    @feedback[@game.turn_count] = feedback
   end
 end
 
 # Player class, handles the player actions and states
 class Player
   # should contain name and guess
-  def __init__(name)
+  def initialize(name, game, board, mastermind)
     @name = name
+    @guess = []
+    @game = game
+    @board = board
+    @mastermind = mastermind
+  end
+  attr_accessor :guess
+
+  # player input method, handles the player input
+  def player_input
+    puts 'Please enter your guess or type help for more information'
+    input = gets.chomp.downcase
+    case input
+    when 'quit' || 'q' # quit the game
+      @game.quit
+    when 'restart' || 'r' # restart the game
+      @game.restart
+    when 'help' || 'h' # display the help menu
+      @game.help
+    else # otherwise it is a guess and should be validated
+      validate_guess(input)
+    end
+  end
+
+  # validate guess method
+  def validate_guess(guess_tmp)
+    # should be a sequence of 4 colours separated by spaces
+    # each value should be one of the 6 allowed colours
+    # accepted values should be stored in the guess array
+    guess_tmp.split(' ').each do |color|
+      if %w[red green blue yellow orange purple].include?(color)
+        @guess.push(color)
+      else
+        invalid_guess
+      end
+    end
+    # should have 4 values in @guess
+    if @guess.length != 4
+      invalid_guess
+    end
+    valid_guess
+  end
+
+  #valid guess method
+  def valid_guess
+    @mastermind.code_response(@guess)
+    @board.receive_guess(@guess)
+    @game.turn_count += 1
     @guess = []
   end
 
-  def player_guess
-    # get the guess from the player
-    # should be a sequence of 4 colours separated by spaces
-    guess_tmp = gets.chomp.split(' ')
-    # should be validated
-    if guess_tmp.length != 4
-      puts "Invalid guess, please enter 4 colours separated by spaces /n
-            (red, green, blue, yellow, orange, purple)"
-      player_guess
-    # should be added to the guess array
-    else
-      @guess = guess_tmp
-      @turn_count += 1
-    end
+  # invalid guess method
+  def invalid_guess
+    puts "Invalid guess, please enter 4 colours separated by spaces /n
+              (red, green, blue, yellow, orange, purple)"
+        @guess = []
+        player_input
   end
 end
 
 # peg class, handles the color and position of each peg
 class Peg
-  def __init__(color, _position)
+  def initialize(color, _position)
     @color = color
     @position = nil
   end
@@ -81,26 +194,43 @@ class Peg
   attr_reader :color
 end
 
-# colours class, handles the possible colours of the pegs
-class Colours
+# key peg class, represents the feedback from the code
+class KeyPeg
 end
 
 # Mastermind class, sets the mystery code and handles the feedback
 class Mastermind
-  def __init__
+  def initialize
     @code = []
     @feedback = []
   end
+  
+  attr_reader :feedback
 
-  def set_code
+  def generate_code
     # generate the code
     # should be a sequence of 4 colours
     # will be randomly generated but for now is hard coded
     # should be stored in the code array
-    @code = ['red', 'green', 'blue', 'yellow']
+    @code = %w[red green blue yellow]
+  end
+
+  def code_response(guess)
+    #clear the feedback array
+    @feedback = []
+    # check the guess against the code
+    @code.each_with_index do |color, index|
+      if guess[index] == color
+        @feedback.push('black')
+      elsif guess.include?(color)
+        @feedback.push('white')
+      else
+        @feedback.push(' ')
+      end
+    end
   end
 end
 
 # Start the game
-game = Mastermind.new
+game = Game.new
 game.play
